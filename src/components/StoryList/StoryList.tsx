87c953aa66c8a489f6fce81ebedb8900dc8b9f54
ananthrajsingh <ananthraj.singh@gmail.com>
2020-08-31 23:16:07 +0530

    Show temporary translucent overlay on story click

--- src/components/StoryList/StoryList.tsx
+++ src/components/StoryList/StoryList.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './StoryList.scss';
 import { CirclePlay } from 'grommet-icons';
-import { For } from 'react-extras';
+import {For, If} from 'react-extras';
 import Avatar, { AvatarSize } from '../Avatar/Avatar';
 import {getDummyUser, User} from "../../model/User";
 
 const StoryList: React.FC = () => {
 
+    let [showStory, setShowStory] = useState<User | null>(null)
+
     function onAvatarClick(user: User) {
         console.log(user.firstName + " clicked")
+        setShowStory(user)
+        setTimeout(
+            function () {
+                setShowStory(null)
+            }, 2000
+        )
     }
 
-    return <div className={'flex flex-col justify-start items-center space-y-8 children:w-full'}>
+    // removing space-y-8 since  it hindered with story with margin
+    return <div className={'flex flex-col justify-start items-center children:w-full'}>
         <div className={'flex flex-row justify-between items-center'}>
             <p className={'font-bold text-xl'}>Stories</p>
             <div className={'flex flex-row justify-end items-center space-x-4 w-full children:cursor-pointer'}>
@@ -19,8 +28,15 @@ const StoryList: React.FC = () => {
                 <p className={'text-base'}>Watch all</p>
             </div>
         </div>
+        <If condition={showStory !== null}>
+            <div className={'w-full h-full inset-0 bg-black absolute z-50 bg-opacity-90'}>
+
+            </div>
+        </If>
+
+        {/*added mt-8 to compensate for removal of space-y-8 from root*/}
         <div
-            className={'flex flex-row justify-start items-center space-x-4 max-w-full overflow-x-auto overflow-y-hidden'}>
+            className={'flex flex-row justify-start items-center space-x-4 mt-8 max-w-full overflow-x-auto overflow-y-hidden'}>
             <For of={Array.from(Array(25).keys())} render={(item, index) =>
                 <Avatar
                         key={index}
