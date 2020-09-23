18e371bef40ebbd247ce779f929bb83f77beef75
ananthrajsingh <ananthraj.singh@gmail.com>
2020-09-23 20:48:37 +0530

    Add elements to Profile

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -14,17 +14,33 @@ interface ProfileProps extends GlobalProps {
 
 const Profile: React.FC<ProfileProps> = (props: ProfileProps) => {
     return <div>
-        <div className={'flex flex-row h-screen w-full overflow-hidden items-center children:h-full space-y-10 pt-12 px-8'}>
-        <FeedList
-        />
-        <ProfileInfo
-            className={'ml-8'}
-            user={props.user}
-            postCount={props.postCount}
-            followerCount={props.followerCount}
-            followingCount={props.followingCount}
-        />
-
+        <div
+            className={'flex flex-row h-screen w-full overflow-hidden items-center children:h-full space-y-10 pt-12 px-8'}>
+            <div>
+                <p className={'text-2xl font-bold mb-8'}>
+                    POSTS
+                </p>
+                <div
+                    className={'h-screen w-full overflow-hidden children:h-full'}
+                >
+                    <FeedList/>
+                </div>
+            </div>
+            <div
+                className={'ml-8 flex flex-col'}
+            >
+                <ProfileInfo
+                    user={props.user}
+                    postCount={props.postCount}
+                    followerCount={props.followerCount}
+                    followingCount={props.followingCount}
+                />
+                <p
+                    className={'mt-4'}
+                >
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                </p>
+            </div>
         </div>
     </div>
 }
